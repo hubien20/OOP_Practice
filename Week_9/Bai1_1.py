43cfa4d4129f4f755bef88ffd31744b2773d4298
hubien <25112017@st.vju.ac.vn>
2026-04-23 14:02:50 +0700

    Update Bai1_1.py

--- Week_9/Bai1_1.py
+++ Week_9/Bai1_1.py
@@ -3,10 +3,22 @@ class Point:
     def __init__(self, x=0, y=1):
         self.__x = x
         self.__y = y
-
+    def read(self):
+        self.__x = int(input("Nhap x: "))
+        self.__y = int(input("Nhap y: "))
+    def display(self):
+        print(f"({self.__x}, {self.__y})")
+    def move(self, dx, dy):
+        self.__x += dx
+        self.__y += dy
+    def getX(self):
+        return self.__x
+    def getY(self):
+        return self.__y
     def distance(self, other):
         return math.sqrt((self.__x - other.__x) ** 2 + (self.__y - other.__y) ** 2)
-
-p=Point(x, y)
-p.print()
+    
+x, y = map(int, input().split())
+p = Point(x, y)
+p.display()
 print(p.distance())
