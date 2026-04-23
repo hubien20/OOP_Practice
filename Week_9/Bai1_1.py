13c056edb82876c57e20ff04d51bda1a11ccfd6b
hubien <25112017@st.vju.ac.vn>
2026-04-23 13:54:47 +0700

    Update Bai1_1.py

--- Week_9/Bai1_1.py
+++ Week_9/Bai1_1.py
@@ -1,8 +1,12 @@
 import math
 class Point:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+    def __init__(self, x=0, y=1):
+        self.__x = x
+        self.__y = y
 
     def distance(self, other):
-        return math.sqrt((self.x - other.x) ** 2 + (self.y - other.y) ** 2)
+        return math.sqrt((self.__x - other.__x) ** 2 + (self.__y - other.__y) ** 2)
+
+p=Point(x, y)
+p.print()
+print(p.distance())
