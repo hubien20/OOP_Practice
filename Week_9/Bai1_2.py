4920a8bb5672b294a1cbc63dbb1d5331d3658262
hubien <25112017@st.vju.ac.vn>
2026-05-07 14:29:09 +0700

    Update Bai1_2.py

--- Week_9/Bai1_2.py
+++ Week_9/Bai1_2.py
@@ -1,17 +1,15 @@
 import math
+
 class Point:
+
     def __init__(self, x=0, y=1):
         self.__x = x
         self.__y = y
     def read(self):
-        try:
-            line = input("Nhập tọa độ (x y): ")
-            data = line.split()
-            self.__x = int(data[0])
-            self.__y = int(data[1])
-        except (IndexError, ValueError):
-            print("Lỗi: Vui lòng nhập đúng định dạng 2 số nguyên cách nhau bởi khoảng trắng!")  
-    def print(self):    
+        x, y = map(int, input().split())
+        self.__x = x
+        self.__y = y
+    def print(self):
         print(f"({self.__x}, {self.__y})")
     def move(self, dx, dy):
         self.__x += dx
@@ -23,11 +21,9 @@ class Point:
     def distance(self, P=None):
         if P is None:
             return math.sqrt(self.__x**2 + self.__y**2)
-        else:
-            dx = self.__x - P.getX()
-            dy = self.__y - P.getY()
-            return math.sqrt(dx**2 + dy**2)
-
+        dx = self.__x - P.getX()
+        dy = self.__y - P.getY()
+        return math.sqrt(dx**2 + dy**2)
 class PointTest:
     def testCase(self):
         A = Point()
@@ -38,6 +34,6 @@ class PointTest:
         B.move(1, 1)
         B.print()
         print(B.distance())
-if _name_ == "_main_":
+if __name__ == "__main__":
     t = PointTest()
     t.testCase()
