9d2acb9b2267f5b5c0ba07878149d3d3fd003b62
hubien <25112017@st.vju.ac.vn>
2026-05-21 05:14:34 +0700

    Create Bai5_2.py

---
+++ OOP_nang_cao/Bai5_2.py
@@ -0,0 +1,20 @@
+class Vector2D:
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
+    def __str__(self):
+        return f"({self.x}, {self.y})"
+    def __add__(self, other):
+        if isinstance(other, Vector2D):
+            return Vector2D(self.x + other.x, self.y + other.y)
+        return NotImplemented
+    def __sub__(self, other):
+        if isinstance(other, Vector2D):
+            return Vector2D(self.x - other.x, self.y - other.y)
+        return NotImplemented
+    def __neg__(self):
+        return Vector2D(-self.x, -self.y)
+    def __eq__(self, other):
+        if isinstance(other, Vector2D):
+            return self.x == other.x and self.y == other.y
+        return False
